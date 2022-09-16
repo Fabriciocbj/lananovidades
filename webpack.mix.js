daa028c85bb3b6faf0e68b527556aa8aeba2bdcb
fabricioguimaraes <fabricio.network@gmail.com>
2022-09-15 23:05:54 -0300

    no message

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,5 @@ const mix = require('laravel-mix');
 
  mix
 
- .sass('resources/sass/app.scss', 'public/site/style.css')
+ .scripts('node_modules/jquery/dist/jquery.js', 'public/site/jquery.js')
+ .scripts('node_modules/bootstrap/dist/js/bootstrap.bundle.js', 'public/site/bootstrap.js')
